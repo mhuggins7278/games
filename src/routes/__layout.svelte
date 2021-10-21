8c76e61016e64b09bdd96ad7704d173e591c42ea
Mark Huggins <mhuggins@glgroup.com>
2021-10-21 10:13:27 -0400

    setting up tailwind

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Nav from '../components/nav.svelte';
+</script>
+
+<div class="p-8 max-w-6xl mx-auto body">
+	<Nav />
+	<div class="flex justify-center w-full">
+		<slot />
+	</div>
+</div>
+
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+  
+</style>
