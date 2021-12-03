721228ab959cecf84735317012d3f15540862a12
Mark Huggins <mhuggins@glgroup.com>
2021-12-02 22:42:28 -0500

    use netlify for deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,18 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-netlify'
 
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/games'
-		}
-	}
-};
+  kit: {
+    target: '#svelte',
+    adapter: adapter(),
+  }
+}
 
-export default config;
+export default config
