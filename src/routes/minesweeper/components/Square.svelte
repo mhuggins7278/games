d77d84da8500c9f5e7c637891bccc9a742720ad5
Mark Huggins <mhuggins@glgroup.com>
2021-12-29 13:47:07 -0500

    revealing all squares now wins

--- src/routes/minesweeper/components/Square.svelte
+++ src/routes/minesweeper/components/Square.svelte
@@ -1,5 +1,5 @@
 <script>
-  export let square;
+  export let square
   import {
     flags,
     flagsLeft,
@@ -8,119 +8,128 @@
     isGameOver,
     board,
     result
-  } from '../lib/stores';
+  } from '../lib/stores'
 
-import {getSquarePosition} from '../lib/game'
+  import {getSquarePosition} from '../lib/game'
 
   function handleClick(square, event) {
     //if the game is over or the square was already clicked return
-    if ($isGameOver || square.checked || square.flag) return;
+    if ($isGameOver || square.checked || square.flag) return
     //if the function was called from checkNeighbouringSquares and the value is a bomb return
-    if (!event && square.value === 'bomb') return;
-    const localBoard = [...$board];
+    if (!event && square.value === 'bomb') return
+    const localBoard = [...$board]
 
     if (square.value === 'valid') {
-      localBoard[square.id].displayValue = square.count > 0 ? square.count : '';
-      localBoard[square.id].checked = true;
-      $board = [...localBoard];
+      localBoard[square.id].displayValue = square.count > 0 ? square.count : ''
+      localBoard[square.id].checked = true
+      $board = [...localBoard]
       //only recurse if the square has no adjecent bombs
-      if (square.count === 0) checkNeighbouringSquares(square);
+      if (square.count === 0) return checkNeighbouringSquares(square)
     } else {
-      bombClicked(square.id);
+      bombClicked(square.id)
     }
+    checkForWin()
   }
 
   function bombClicked(id) {
-    $result = '🤯';
-    $isGameOver = true;
+    $result = '🤯'
+    $isGameOver = true
 
     //show ALL the bombs
     $board = [
       ...$board.map((square) => {
         if (square.value === 'bomb') {
-          square.displayValue = '💣';
-          square.checked = square.id === id ? true : false;
+          square.displayValue = '💣'
+          square.checked = square.id === id ? true : false
         }
-        return square;
+        return square
       })
-    ];
+    ]
   }
 
   function checkNeighbouringSquares(square) {
-    const { id } = square;
-    const { isLeftEdge, isRightEdge, isTopRow, isBottomRow } = getSquarePosition(id)
+    const {id} = square
+    const {isLeftEdge, isRightEdge, isTopRow, isBottomRow} =
+      getSquarePosition(id)
 
     setTimeout(() => {
       //check the square to the left
       if (!isLeftEdge) {
-        const nextSquare = $board[id - 1];
-        handleClick(nextSquare);
+        const nextSquare = $board[id - 1]
+        handleClick(nextSquare)
       }
       // check the square to the right
       if (!isRightEdge) {
-        const nextSquare = $board[id + 1];
-        handleClick(nextSquare);
+        const nextSquare = $board[id + 1]
+        handleClick(nextSquare)
       }
       //check the square below
       if (!isBottomRow) {
-        const nextSquare = $board[id + $width];
-        handleClick(nextSquare);
+        const nextSquare = $board[id + $width]
+        handleClick(nextSquare)
       }
       //check the square above the current square
       if (!isTopRow) {
-        const nextSquare = $board[id - $width];
-        handleClick(nextSquare);
+        const nextSquare = $board[id - $width]
+        handleClick(nextSquare)
       }
       //check the square up and to the right
       if (!isTopRow && !isRightEdge) {
-        const nextSquare = $board[id + 1 - $width];
-        handleClick(nextSquare);
+        const nextSquare = $board[id + 1 - $width]
+        handleClick(nextSquare)
       }
       //check the square up and to the left
       if (!isTopRow && !isLeftEdge) {
-        const nextSquare = $board[id - 1 - $width];
-        handleClick(nextSquare);
+        const nextSquare = $board[id - 1 - $width]
+        handleClick(nextSquare)
       }
       //check the square below and to the left
       if (!isBottomRow && !isLeftEdge) {
-        const nextSquare = $board[id - 1 + $width];
-        handleClick(nextSquare);
+        const nextSquare = $board[id - 1 + $width]
+        handleClick(nextSquare)
       }
       //check the square below and to the right
-      if (!isBottomRow&& !isRightEdge) {
-        const nextSquare = $board[id + 1 + $width];
-        handleClick(nextSquare);
+      if (!isBottomRow && !isRightEdge) {
+        const nextSquare = $board[id + 1 + $width]
+        handleClick(nextSquare)
       }
-    }, 10);
+    }, 10)
   }
 
   //toggle Flag with right click
   function toggleFlag(square) {
-    let localFlagCount = $flags;
-    if ($isGameOver) return;
+    let localFlagCount = $flags
+    if ($isGameOver) return
     if (!square.checked && $flags < $bombAmount) {
-      const localBoard = $board;
-      localBoard[square.id].flag = !square.flag;
-      localBoard[square.id].displayValue = square.flag ? '🚩' : '';
-      square.flag ? localFlagCount++ : localFlagCount--;
-      $flags = localFlagCount;
-      $board = [...localBoard];
-      if (square.flag) checkForWin();
+      const localBoard = $board
+      localBoard[square.id].flag = !square.flag
+      localBoard[square.id].displayValue = square.flag ? '🚩' : ''
+      square.flag ? localFlagCount++ : localFlagCount--
+      $flags = localFlagCount
+      $board = [...localBoard]
+      if (square.flag) checkForWin()
     }
   }
 
   //check for win
   function checkForWin() {
-    if ($flagsLeft > 0) return;
     ///simplified win argument
-    const matches = $board.reduce((acc, square) => {
-      if (square.flag && square.value === 'bomb') acc++;
-      return acc;
-    }, 0);
+    const flaggedSquares = $board.reduce((acc, square) => {
+      if (square.flag && square.value === 'bomb') acc++
+      return acc
+    }, 0)
 
-    if (matches === $bombAmount) {
-      $result = '😅';
-      $isGameOver = true;
+    const revealedSquares = $board.reduce((acc, square) => {
+      if (square.checked && square.value === 'valid') acc++
+      return acc
+    }, 0)
+
+    if (
+      flaggedSquares === $bombAmount ||
+      revealedSquares === $board.length - $bombAmount
+    ) {
+      $result = '😅'
+      $isGameOver = true
     }
   }
 </script>
